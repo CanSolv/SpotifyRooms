02759072650b25433793831953da22882eea2281
Onur Ödemiş <onur.odemis@solvoyo.local>
2022-07-28 11:38:49 +0300

    Add initial files

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      Test
+      <span className="icon icon-headphone"></span>
+      <div className="fyp">
+        <img src="https://ticari.tarkett.com.tr/media/img/M/TH_3917011_3707003_3708011_3912011_3914011_800_800.jpg" />
+        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX//wCKxvRFAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/23/Light_green.PNG" />
+        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEUAAP+KeNJXAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import './assets/css/icons.css'
+import './assets/css/fixed-bar.css'
+import './assets/css/explore.css'
+import './assets/css/room-library.css'
+import './assets/css/index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
